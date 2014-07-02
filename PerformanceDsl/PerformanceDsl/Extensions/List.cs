1911c6e1726a8d565cffa65fae0d0fe99fd439e2
TomPallister <tom.pallister@fourth.com>
2014-07-02 13:32:26 +0100

    changes from r9

---
+++ PerformanceDsl/PerformanceDsl/Extensions/List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceDsl.Extensions
+{
+    public static class List
+    {
+        public static IList<T> Clone<T>(this IList<T> listToClone) where T : ICloneable
+        {
+            return listToClone.Select(item => (T)item.Clone()).ToList();
+        }
+
+        public static List<T> Clone<T>(this List<T> listToClone) where T : ICloneable
+        {
+            return listToClone.Select(item => (T)item.Clone()).ToList();
+        }
+    }
+}
